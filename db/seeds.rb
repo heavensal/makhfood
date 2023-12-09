c2336c8b7d2cc55b419a458640cd77ea19605ac5
adam lamouri <adamlamouri.pro@gmail.com>
2023-12-09 17:49:28 +0100

    meilleur selenium

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,26 +25,28 @@ def create_product(driver, wait)
   product = Product.new
 
   begin
-    product.name = wait.until { driver.find_element(css: '.product_title.entry-title') }.text
+    product.name = wait.until { driver.find_element(css: 'h1') }.text
   rescue Selenium::WebDriver::Error::TimeoutError
     product.name = "Pas de nom"
   end
   puts product.name
 
-  # begin
-  #   product.sku = wait.until { driver.find_element(css: 'div.grey-30-color > span:nth-child(1) > span:nth-child(2)').text }
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.sku = nil
-  # end
-
-  # begin
-  #   product.ean = wait.until { driver.find_element(xpath: '//div[contains(@class, "grey-30-color")]/span[2]/span[2]').text.to_i }
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.ean = nil
-  # end
+  begin
+    product.sku = wait.until { driver.find_element(css: 'div.grey-30-color > span:nth-child(1) > span:nth-child(2)').text }
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.sku = nil
+  end
+  puts product.sku
 
   begin
-    img = wait.until { driver.find_element(css: 'figure img').attribute('src') }
+    product.ean = wait.until { driver.find_element(xpath: '//div[contains(@class, "grey-30-color")]/span[2]/span[2]').text.to_i }
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.ean = nil
+  end
+  puts product.ean
+
+  begin
+    img = wait.until { driver.find_element(css: '#catalog_product_view-media_gallery img').attribute('src') }
     if img
       product.link = img
     end
@@ -53,93 +55,102 @@ def create_product(driver, wait)
   end
   puts product.link
 
+  # begin
+  #   product.description = ''
+  #   all_p = wait.until { driver.find_elements(css: '.d_none.tablet:d_block > p') }
+  #   all_p.each do |p|
+  #     product.description += p.text
+  #   end
+  # rescue Selenium::WebDriver::Error::TimeoutError
+  #   product.description = "Pas de description"
+  # end
+  # puts product.description
+
   begin
-    product.description = ''
-    all_p = wait.until { driver.find_elements(css: '.woocommerce-product-details__short-description p') }
-    all_p.each do |p|
-      product.description += p.text
-    end
+    span_element = wait.until { driver.find_element(css: 'span.final-price.primary-color').text }
+    match = span_element.match(/(\d+\.\d+)/)
+    product.unit_price = match[1].to_f if match
+    texte_apres_slash_match = span_element.match(/\/\s*(.+)/)
+    product.unit = texte_apres_slash_match[1] if texte_apres_slash_match
   rescue Selenium::WebDriver::Error::TimeoutError
-    product.description = "Pas de description"
+    product.unit_price = nil
+    product.unit = nil
   end
-  puts product.description
+  puts product.unit_price
+  puts product.unit
 
-  # begin
-  #   span_element = wait.until { driver.find_element(css: 'span.final-price.primary-color').text }
-  #   match = span_element.match(/(\d+\.\d+)/)
-  #   product.unit_price = match[1].to_f if match
-  #   texte_apres_slash_match = span_element.match(/\/\s*(.+)/)
-  #   product.unit = texte_apres_slash_match[1] if texte_apres_slash_match
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.unit_price = nil
-  #   product.unit = nil
-  # end
+  begin
+    product.box = wait.until { driver.find_element(css: '.d_flex.ai-center.size-18.text-uppercase.m-b-1 > span:nth-child(2)').text }
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.box = nil
+  end
+  puts product.box
 
-  # begin
-  #   product.box = wait.until { driver.find_element(css: '.d_flex.ai-center.size-18.text-uppercase.m-b-1 > span:nth-child(2)').text }
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.box = nil
-  # end
+  begin
+    element = wait.until { driver.find_element(xpath: '(//div[@class="d_flex ai-center col-gap-5"])[2]//span[1]').text }
+    match = element.match(/(\d+\.\d+)/)
+    product.price_per_unit = match[1].to_f if match
+    match = element.match(/\/\s*(.+)/)
+    product.mesure_unit = match[1] if match
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.price_per_unit = nil
+    product.mesure_unit = nil
+  end
+  puts product.price_per_unit
+  puts product.mesure_unit
 
-  # begin
-  #   element = wait.until { driver.find_element(xpath: '(//div[@class="d_flex ai-center col-gap-5"])[2]//span[1]').text }
-  #   match = element.match(/(\d+\.\d+)/)
-  #   product.price_per_unit = match[1].to_f if match
-  #   match = element.match(/\/\s*(.+)/)
-  #   product.mesure_unit = match[1] if match
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.price_per_unit = nil
-  #   product.mesure_unit = nil
-  # end
+  begin
+    pourcentage_element = wait.until { driver.find_element(css: 'div.d_flex.ai-center.col-gap-5:nth-child(2) span.size-12.text-uppercase:nth-child(2) span:nth-child(2)').text }
+    product.tva = pourcentage_element.gsub('%', '').to_f
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.tva = nil
+  end
+  puts product.tva
 
-  # begin
-  #   pourcentage_element = wait.until { driver.find_element(css: 'div.d_flex.ai-center.col-gap-5:nth-child(2) span.size-12.text-uppercase:nth-child(2) span:nth-child(2)').text }
-  #   product.tva = pourcentage_element.gsub('%', '').to_f
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.tva = nil
-  # end
+  begin
+    match = wait.until { driver.find_element(css: '.d_flex.ai-center.col-gap-30.m-b-3.size-12 > span:nth-child(1)').text }
+    match = match.match(/(\d+\.\d+)/)
+    product.ht_box_price = match[1].to_f if match
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.ht_box_price = nil
+  end
+  puts product.ht_box_price
 
-  # begin
-  #   match = wait.until { driver.find_element(css: '.d_flex.ai-center.col-gap-30.m-b-3.size-12 > span:nth-child(1)').text }
-  #   match = match.match(/(\d+\.\d+)/)
-  #   product.ht_box_price = match[1].to_f if match
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.ht_box_price = nil
-  # end
-
-  # begin
-  #   match = wait.until { driver.find_element(css: '.d_flex.ai-center.col-gap-30.m-b-3.size-12 > span:nth-child(2)').text }
-  #   match = match.match(/(\d+\.\d+)/)
-  #   product.ttc_box_price = match[1].to_f if match
-  # rescue Selenium::WebDriver::Error::TimeoutError
-  #   product.ttc_box_price = nil
-  # end
+  begin
+    match = wait.until { driver.find_element(css: '.d_flex.ai-center.col-gap-30.m-b-3.size-12 > span:nth-child(2)').text }
+    match = match.match(/(\d+\.\d+)/)
+    product.ttc_box_price = match[1].to_f if match
+  rescue Selenium::WebDriver::Error::TimeoutError
+    product.ttc_box_price = nil
+  end
+  puts product.ttc_box_price
 
   product.save!
   puts "#{product.name} créé !"
 end
 
-driver.navigate.to 'https://central-hal.com/boutique/'
+driver.navigate.to 'https://www.centrale-ethnique.com/catalogsearch/result/?q=gulnur'
 driver.manage.timeouts.implicit_wait = 10
 main_window = driver.window_handle
 i = 1
 
-loop do
-  links = wait.until { driver.find_elements(css: '.image-block > a') }
-  links.each do |a|
-    driver.execute_script("window.open('#{a.attribute('href')}');")
-    driver.switch_to.window(driver.window_handles.last)
-    driver.manage.timeouts.implicit_wait = 5
-    create_product(driver, wait)
-    driver.close
-    driver.switch_to.window(main_window)
-  end
-  i += 1
-  driver.navigate.to "https://central-hal.com/boutique/page/#{i}"
-  break if i > 34
+links = wait.until { driver.find_elements(css: '.product_infos.text-center > a') }
+links.each do |a|
+  driver.execute_script("window.open('#{a.attribute('href')}');")
+  driver.switch_to.window(driver.window_handles.last)
+  driver.manage.timeouts.implicit_wait = 5
+  create_product(driver, wait)
+  driver.close
+  driver.switch_to.window(main_window)
 end
 
 driver.quit
+# loop do
+# end
+
+# i += 1
+# driver.navigate.to "https://central-hal.com/boutique/page/#{i}"
+# break if i > 34
 
   # Mise à jour et vérification du bouton "Suivant"
 #   next_buttons = driver.find_elements(xpath: '//*[local-name()="svg" and contains(@class, "w-20p h-20p")]')
